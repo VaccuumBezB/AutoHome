ec84056542a134f1b4dc3f79c41b86a462934ea3
Vac'cuum <107546275+VaccuumBezB@users.noreply.github.com>
2025-06-02 17:10:22 +0300

    Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -4,6 +4,9 @@
 # THIS SCRIPT WILL AUTOMATICALLY CONFIGURE YOUR ORANGE PI #
 ###########################################################
 
+sudo apt install git
+sudo apt install nodejs
+
 git clone https://github.com/zhaolei/WiringOP.git -b h3
 
 cd WiringOP
